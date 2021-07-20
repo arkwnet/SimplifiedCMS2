9d884faa9513298befb28167fcbd25ad8c2f543a
Sora Arakawa <sr@arkw.jp>
2021-07-20 22:46:45 +0900

    Update sample data

--- backend/create.php
+++ backend/create.php
@@ -4,10 +4,11 @@ if (isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER["HTTP_X_REQU
 	if (!file_exists($file)) {
 		if ($_GET["type"] == "file") {
 			touch($file);
+			chmod($file, 0666);
 		} elseif ($_GET["type"] == "directory") {
-			mkdir($file);
+			mkdir($file, 0777);
+			chmod($file, 0777);
 		}
-		chmod($file, 0666);
 	} else {
 		echo("EXIST");
 		exit();
